170f0fc59de61f3b8a12af238cd4b1deaaaa659a
Hanna Assaf <145717934+HannaAssaf@users.noreply.github.com>
2025-09-24 22:38:54 +0100

    start

--- src/server.js
+++ src/server.js
@@ -1,1 +1,12 @@
-// code
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hello world!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
