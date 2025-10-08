5fb19e10a2f4bc6c1ea88baacf09e82640b6b1bf
Hanna Assaf <145717934+HannaAssaf@users.noreply.github.com>
2025-10-08 21:31:58 +0100

    copied-all-files-from-branch-02

--- src/server.js
+++ src/server.js
@@ -1,1 +1,32 @@
-// code
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import { connectMongoDB } from './db/connectMongoDB.js';
+import notesRoutes from './routes/notesRoutes.js';
+import { errorHandler } from './middleware/errorHandler.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { logger } from './middleware/logger.js';
+
+const app = express();
+const PORT = process.env.PORT ?? 3030;
+
+app.use(logger);
+app.use(
+  express.json({
+    type: ['application/json', 'application/vnd.api+json'],
+    limit: '100kb',
+  }),
+);
+app.use(cors());
+
+app.use(notesRoutes);
+
+app.use(notFoundHandler);
+
+app.use(errorHandler);
+
+await connectMongoDB();
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
