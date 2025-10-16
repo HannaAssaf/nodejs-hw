881e2ddd94794679708f5ac7d10e6e94167c2180
Hanna Assaf <145717934+HannaAssaf@users.noreply.github.com>
2025-10-16 23:16:27 +0100

    feat: add refresh

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,6 +3,7 @@ import { celebrate } from 'celebrate';
 import {
   loginUser,
   logoutUser,
+  refreshUserSession,
   registerUser,
 } from '../controllers/authController.js';
 import {
@@ -14,6 +15,6 @@ const router = Router();
 
 router.post('/auth/register', celebrate(registerUserSchema), registerUser);
 router.post('/auth/login', celebrate(loginUserSchema), loginUser);
-// router.post('/auth/refres', celebrate(loginUserSchema), loginUser);
+router.post('/auth/refresh', refreshUserSession);
 router.post('/auth/logout', logoutUser);
 export default router;
