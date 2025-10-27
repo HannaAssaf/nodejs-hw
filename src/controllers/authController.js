3404d8718b7077a0284c447ff9b2ca2aedff15d9
Hanna Assaf <145717934+HannaAssaf@users.noreply.github.com>
2025-10-27 17:08:05 +0000

    fix: link

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -115,7 +115,7 @@ export const requestResetEmail = async (req, res) => {
   const template = handlebars.compile(templateSource);
   const html = template({
     name: user.username,
-    link: `${process.env.FRONTEND_DOMAIN}/reset-password?token=${resetToken}`,
+    link: `${process.env.FRONTEND_DOMAIN}/reset-password-email?token=${resetToken}`,
   });
 
   try {
